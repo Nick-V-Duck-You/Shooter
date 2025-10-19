df1d77dc0f801706dd2eb3c1d8b77ad2a38df2ea
Nick-V-Duck-You <valentinedead320@gmail.com>
2025-10-19 20:13:55 +0300

    added npc movement waypoint, needed fix for reverse direction

--- Assets/ShooterFolder/Scripts/NPCMovement.cs
+++ Assets/ShooterFolder/Scripts/NPCMovement.cs
@@ -6,6 +6,9 @@ public class NPCMovement : MonoBehaviour
     public GameObject playerObject;
     public GameObject particleSystemObject;
 
+    public Transform[] waypoints;
+    private int currentWaypointIndex = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,10 +17,57 @@ public class NPCMovement : MonoBehaviour
 
     void Update()
     {
-      float speed = 5f;
-      Vector3 targetPosition = playerObject.transform.position;
+      //Vector3 targetPosition = playerObject.transform.position;
 
-      transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+      //transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+      
+      
+      if (currentWaypointIndex < waypoints.Length)
+
+     {
+            // current waypoint
+            Transform target = waypoints[currentWaypointIndex];
+
+            // move to waypoint
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            // rotate to movement direction
+            Vector3 direction = target.position - transform.position;
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            }
+
+            // chek if npc reach current waypoint
+            if (Vector3.Distance(transform.position, target.position) < 0.1f)
+            {
+                currentWaypointIndex++;
+            }
+     }
+     
+     else{
+         while (currentWaypointIndex != 0)
+         {
+             // current waypoint
+            Transform target = waypoints[currentWaypointIndex];
+
+            // move to waypoint
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            // rotate to movement direction
+            Vector3 direction = target.position - transform.position;
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            }
+
+            // chek if npc reach current waypoint
+            if (Vector3.Distance(transform.position, target.position) < 0.1f)
+            {
+                currentWaypointIndex -=1 ;
+            }
+         }
+     }
 
 
     }
--- Assets/ShooterFolder/Scripts/NPCSpawner.cs
+++ Assets/ShooterFolder/Scripts/NPCSpawner.cs
@@ -4,11 +4,14 @@ public class NPCSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject objectToSpawn;
     [SerializeField] private GameObject spawner;
+    public Transform[] waypoints; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InvokeRepeating("Spawn", 2f, 5f); 
+        var npcmovement = objectToSpawn.GetComponent<NPCMovement>();
+        npcmovement.waypoints = waypoints;
     }
 
     // Update is called once per frame
